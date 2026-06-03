cdb7588737a2978efb09bb3a9f7231f38ad521de
Natalia <nataliya.yanovic@mail.ru>
2026-06-03 09:02:44 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const recortarTexto = (texto, limite) => texto.slice(0, limite);
+
+const inputEl = document.getElementById("inputTexto");
+const inputNum = document.getElementById("inputNumero")
+const btnEl = document.getElementById("btnCalcular");
+const resultEl = document.getElementById("resultadoTexto");
+
+btnEl.addEventListener("click", () => {
+    const texto = inputEl.value.trim();
+    const numero = parseInt(inputNum.value);
+
+    if (texto === "" && numero === isNaN(numero) || numero <= 0) {
+        resultEl.textContent = "Error: ¡Los campos no pueden estar vacío!";        
+        resultEl.style.color = "red";
+        return; 
+    }
+
+    resultEl.textContent = `El texto recortado es: ${recortarTexto(texto, numero)}`;
+    resultEl.style.color = "white";
+
+    inputEl.value = "";
+    inputNum.value = "";
+    inputEl.focus();
+});
